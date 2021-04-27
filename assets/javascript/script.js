48355b4fd3ab2f4cdc58fd9fa59a1e33b553b815
raddymaster <antellitocci@gmail.com>
2021-04-26 23:44:24 -0400

    added comments to javascript file for additional clarity

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -18,7 +18,7 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-//Create boolean variables to store user dialog choices
+//Create boolean variables to store user dialog choices and initialize password length at 0
 var inclLowercase = false;
 var inclUppercase = false;
 var inclNumeric = false;
@@ -44,25 +44,28 @@ function presentPrompts()
     inclSpecial = window.confirm("Would you like to include special charcters (! or & for example)? \n\n('Ok' to include. 'Cancel' to exclude.)");
     console.log(inclSpecial);
 
+    //If user fails to select any of the character types, present them with the prompts again.
     if (inclLowercase === false && inclUppercase === false && inclNumeric === false && inclSpecial === false)
     {
       window.confirm("Please be sure to select at least one criteria to generate a password.");
       presentPrompts();
     }
+
     //Get user input for length of password to have
     passwordLength = Number(window.prompt("Please select the length of your password. \n\n(Choose a number between 8 and 128.)", ""));
+    //Log password length for QA testing
     console.log(passwordLength);
+    //If the users response falls outside of acceptable password length range, keep presenting them with the password length selection dialog box until they choose an appropriate length.
     while (passwordLength < 8 || passwordLength > 128)
     {
       passwordLength = Number(window.prompt("Please select the length of your password. \n\nPassword must be between 8 and 128 characters.", ""));
       if(passwordLength >= 8 && passwordLength <=128)
       {
         passwordLength = passwordLength;
-        console.log(passwordLength, inclLowercase, inclUppercase, inclNumeric, inclSpecial);
       }
     }
+    //Begin the password generation phase
     generatePassword();
-
 }
 
 //Generate password
@@ -78,34 +81,39 @@ function generatePassword()
     var availableLowerArray = "abcdefghijklmnopqrstuvwxyz".split("");
     var availableUpperArrary = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
     var availableNumericArray = "0123456789".split("");
-    var availableSpecialArray = "!@#$%^&*()-+=".split("");
+    var availableSpecialArray = "!@#$%^&*()-+=".split(""); //I only used the special characters on the top row of the QWERTY keyboard starting at 1 and ending at =
     
-    //Nest the character arrays into another array
-    var checkAgainstArray = [[availableLowerArray], [availableUpperArrary], [availableNumericArray], [availableSpecialArray]];
+    //Nest the possible character arrays into another array. A psuedo "all available characters" array if you will.
+    var pseudoAllAvailChar = [[availableLowerArray], [availableUpperArrary], [availableNumericArray], [availableSpecialArray]];
 
+    //Check if a user has declined to use a character type. If they have, it will set the nested array at index i to an empty array in the pseudoAllAvailChar array
     for (var i=0; i < userSelectionArray.length; i++)
     {
-      //console.log(userSelectionArray[i].valueOf(i));
         if(userSelectionArray[i].valueOf(i) === false)
         {
-          console.log(userSelectionArray[i].valueOf(i));
-          checkAgainstArray[i] = [];
+          //if false, set the nested array at index i to an empty array.
+          pseudoAllAvailChar[i] = [];
         }
     }
-    //Flatten the nested arrays created in check against array. 2 is used to bring it up two levels (and prevent further nested arrays which causes issues when trying to select 
+    //Flatten the nested arrays created in psuedoAllAvailChar array. 2 is used to bring it up two levels (and prevent further nested arrays which causes issues when trying to select 
     //characters for the password)
-    var availableCharArray = checkAgainstArray.flat(2);
+    var availableCharArray = pseudoAllAvailChar.flat(2);
     console.log(availableCharArray);
     //Generate a random password
     for (var i=0; i < passwordLength; i++)
     {
+      //choose a random number. minimum value (0) is included and maximum value (array.length) is excluded. Exclusion is okay because there is no character at that index # any way.
       var randomNum = Math.floor(Math.random()*availableCharArray.length);
+      //convert the chosen character to a string for addition to the password string.
       var randomArrayValue = availableCharArray[randomNum].toString();
+      //add the random character to the password string.
       generatedPassword += randomArrayValue;
     }
+    //log the password for QA testing
     console.log(generatedPassword);
+
+    //Run the writePassword function to print the password to the screen
     writePassword(generatedPassword);
-    return generatedPassword;
 }
 
 
@@ -120,5 +128,5 @@ function writePassword(genPassword) {
 
 }
 
-// Add event listener to generate button
+// Add event listener to generate button and then present prompts for the user
 generateBtn.addEventListener("click", presentPrompts);
